10ffdbf2c53578ded71b4f1c1f4568b766216a34
nishanpradhan <nishanpradhan500@gmail.com>
2024-03-14 07:29:54 +0545

    pattern

--- pattern.java
+++ pattern.java
@@ -6,8 +6,8 @@ public class pattern {
             for (j = 1; j <= rows - i; j++) {
                 System.out.print(" ");
             }
-            for (j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
+            for (j = 1; j <= i; j++) {
+                System.out.print("* ");
             }
             System.out.print("\n");
         }
