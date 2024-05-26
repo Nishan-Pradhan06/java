b7f4e3854ed0a018612fa6b3e963d1658652e706
nishanpradhan <nishanpradhan500@gmail.com>
2024-05-26 20:15:40 +0545

    java-pratice

---
+++ compareString.java
@@ -0,0 +1,19 @@
+public class compareString {
+    public static void main(String[] args) {
+        String string1 = "java";
+        String string2 = "java";
+        String string3 = "java";
+
+        compareString(string2, string3);
+    }
+
+    public static void compareString(String str1, String str2)
+    {
+        if (str1.equals(str2)) {
+            System.out.println("The String are equals");
+        }
+        else {
+            System.out.println("The Strings are not equal");
+        }
+    }
+}
---
+++ stringReverse.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class stringReverse {
+  public static void main(String[] args) {
+      String str = "java the hawa";
+      StringBuffer s = new StringBuffer(str);
+      s.reverse();
+      System.out.println(s);
+  }
+}
