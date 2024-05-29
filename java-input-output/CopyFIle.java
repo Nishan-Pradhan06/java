6172bdeb8446e4da4d3909befb794d27ca3d03c6
nishanpradhan <nishanpradhan500@gmail.com>
2024-05-29 15:12:17 +0545

    thread/i-o

--- java-input-output/CopyFIle.java
+++ java-input-output/CopyFIle.java
@@ -1,3 +1,33 @@
-public class CopyFIle {
-    
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyFile {
+
+    public static void main(String[] args) {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        try {
+            in = new FileInputStream("input.txt");
+            out = new FileOutputStream("output.txt");
+
+            int c;
+            while ((c = in.read()) != -1) {  // Corrected the condition to check for end of file
+                out.write(c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();  // Added exception handling
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();  // Added exception handling for closing streams
+            }
+        }
+    }
 }
