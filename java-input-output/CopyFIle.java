821aa369b65782151655cd5c908c9b9b29f188ec
nishanpradhan <nishanpradhan500@gmail.com>
2024-05-29 15:20:38 +0545

    java

--- java-input-output/CopyFIle.java
+++ java-input-output/CopyFIle.java
@@ -2,7 +2,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class CopyFile {
+public class CopyFIle {
 
     public static void main(String[] args) {
         FileInputStream in = null;
---
+++ variable/variable.java
@@ -0,0 +1,7 @@
+public class variable {
+    public static void main(String[] args)
+    {
+        int localVariable = 10; //local variable
+        System.out.println(localVariable);
+    }
+}
